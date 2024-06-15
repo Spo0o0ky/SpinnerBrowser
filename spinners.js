f3a72e16459a6f69b2b85ab08967d6e03c65a796
Spooooky <145117953+Spo0o0ky@users.noreply.github.com>
2024-06-15 19:12:25 +0100

    Add files via upload

---
+++ spinners.js
@@ -0,0 +1,155 @@
+let spinners = [
+    ["Vanilla (Pure White)","danger/spinners/Spooooky/AssetPack/vanilla","_whitepure", true],
+    ["Vanilla (Cool White)", "danger/spinners/Spooooky/AssetPack/vanilla", "_whitecool", true],
+    ["Vanilla (Black)", "danger/spinners/Spooooky/AssetPack/vanilla", "_black", true],
+    ["Vanilla (Dark Teal)", "danger/spinners/Spooooky/AssetPack/vanilla", "_darkteal", true],
+    ["Vanilla (Dark Green)", "danger/spinners/Spooooky/AssetPack/vanilla", "_darkgreen", true],
+    ["Vanilla (Green)", "danger/spinners/Spooooky/AssetPack/vanilla", "_green", true],
+    ["Vanilla (Orange)", "danger/spinners/Spooooky/AssetPack/vanilla", "_orange", true],
+    ["Vanilla (Pastel Green)", "danger/spinners/Spooooky/AssetPack/vanilla", "_pastelgreen", true],
+    ["Vanilla (Pastel Orange)", "danger/spinners/Spooooky/AssetPack/vanilla", "_pastelorange", true],
+    ["Vanilla (Pastel Pink)", "danger/spinners/Spooooky/AssetPack/vanilla", "_pastelpink", true],
+    ["Vanilla (Pastel Purple)", "danger/spinners/Spooooky/AssetPack/vanilla", "_pastelpurple", true],
+    ["Vanilla (Purple)", "danger/spinners/Spooooky/AssetPack/vanilla", "_purple", true],
+    ["Vanilla (Yellow)", "danger/spinners/Spooooky/AssetPack/vanilla", "_yellow", true],
+    ["Spikey (Pure White)", "danger/spinners/Spooooky/AssetPack/spikey", "_whitepure", true],
+    ["Spikey (Cool White)", "danger/spinners/Spooooky/AssetPack/spikey", "_whitecool", true],
+    ["Spikey (Black)", "danger/spinners/Spooooky/AssetPack/spikey", "_black", true],
+    ["Spikey (Red)", "danger/spinners/Spooooky/AssetPack/spikey", "_red", true],
+    ["Spikey (Orange)", "danger/spinners/Spooooky/AssetPack/spikey", "_orange", true],
+    ["Spikey (Yellow)", "danger/spinners/Spooooky/AssetPack/spikey", "_yellow", true],
+    ["Spikey (Green)", "danger/spinners/Spooooky/AssetPack/spikey", "_green", true],
+    ["Spikey (Icy)", "danger/spinners/Spooooky/AssetPack/spikey", "_icy", true],
+    ["Spikey (Blue)", "danger/spinners/Spooooky/AssetPack/spikey", "_blue", true],
+    ["Spikey (Purple)", "danger/spinners/Spooooky/AssetPack/spikey", "_purple", true],
+    ["Spikey (Pink)", "danger/spinners/Spooooky/AssetPack/spikey", "_pink", true],
+    ["Vanilla-ish (Pure White)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_whitepure", true],
+    ["Vanilla-ish (Cool White)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_whitecool", true],
+    ["Vanilla-ish (Black)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_black", true],
+    ["Vanilla-ish (Red)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_red", true],
+    ["Vanilla-ish (Orange)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_orange", true],
+    ["Vanilla-ish (Yellow)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_yellow", true],
+    ["Vanilla-ish (Green)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_green", true],
+    ["Vanilla-ish (Blue)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_blue", true],
+    ["Vanilla-ish (Purple)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_purple", true],
+    ["Vanilla-ish (Pink)", "danger/spinners/Spooooky/AssetPack/vanillaish", "_pink", true],
+    ["Gems (Pure White)", "danger/spinners/Spooooky/AssetPack/gems", "_whitepure", true],
+    ["Gems (Cool White)", "danger/spinners/Spooooky/AssetPack/gems", "_whitecool", true],
+    ["Gems (Black)", "danger/spinners/Spooooky/AssetPack/gems", "_black", true],
+    ["Gems (Red)", "danger/spinners/Spooooky/AssetPack/gems", "_red", true],
+    ["Gems (Orange)", "danger/spinners/Spooooky/AssetPack/gems", "_orange", true],
+    ["Gems (Yellow)", "danger/spinners/Spooooky/AssetPack/gems", "_yellow", true],
+    ["Gems (Green)", "danger/spinners/Spooooky/AssetPack/gems", "_green", true],
+    ["Gems (Blue)", "danger/spinners/Spooooky/AssetPack/gems", "_blue", true],
+    ["Gems (Purple)", "danger/spinners/Spooooky/AssetPack/gems", "_purple", true],
+    ["Space Crystal (Blue)", "danger/spinners/Spooooky/AssetPack/spacecrystal", "_blue", true],
+    ["Space Crystal (Orange)", "danger/spinners/Spooooky/AssetPack/spacecrystal", "_orange", true],
+    ["Space Crystal (Purple)", "danger/spinners/Spooooky/AssetPack/spacecrystal", "_purple", true],
+    ["Space Crystal (Low Contrast)", "danger/spinners/Spooooky/AssetPack/spacecrystal", "_lowcontrast", true],
+    ["Forest (Green)", "danger/spinners/Spooooky/AssetPack/forest", "_green", true],
+    ["Forest (Light Green)", "danger/spinners/Spooooky/AssetPack/forest", "_lightgreen", true],
+    ["Forest (Orange)", "danger/spinners/Spooooky/AssetPack/forest", "_orange", true],
+    ["Forest (Red)", "danger/spinners/Spooooky/AssetPack/forest", "_red", true],
+    ["Forest (Pink)", "danger/spinners/Spooooky/AssetPack/forest", "_pink", true],
+    ["Forest (Blue)", "danger/spinners/Spooooky/AssetPack/forest", "_blue", true],
+    ["Thorns (White)", "danger/spinners/Spooooky/AssetPack/thorns", "_white", true],
+    ["Thorns (Green)", "danger/spinners/Spooooky/AssetPack/thorns", "_green", true],
+    ["Thorns (Red)", "danger/spinners/Spooooky/AssetPack/thorns", "_red", true],
+    ["Thorns (Pink)", "danger/spinners/Spooooky/AssetPack/thorns", "_pink", true],
+    ["Vines (Green)", "danger/spinners/Spooooky/AssetPack/vines", "_green", true],
+    ["Vines (White Flowers)", "danger/spinners/Spooooky/AssetPack/vines", "_whiteflowers", true],
+    ["Vines (Red)", "danger/spinners/Spooooky/AssetPack/vines", "_red", true],
+    ["Barbs (Clean)", "danger/spinners/Spooooky/AssetPack/barbs", "_clean", true],
+    ["Barbs (Rusty)", "danger/spinners/Spooooky/AssetPack/barbs", "_rusty", true],
+    ["Bones", "danger/spinners/Spooooky/AssetPack/bones", "_bones", true],
+    ["Nebula", "danger/spinners/Spooooky/AssetPack/nebula", "_pink", true],
+    ["Casino (Suits)", "danger/spinners/Spooooky/AssetPack/casino", "_suits", true],
+    ["Casino (Chips)", "danger/spinners/Spooooky/AssetPack/casino", "_chips", true],
+    ["Cutlery", "danger/spinners/Spooooky/AssetPack/cutlery", "_cutlery", true],
+    ["Noughts and Crosses", "danger/spinners/Spooooky/AssetPack/noughtsandcrosses", "_red", true],
+    ["Music Notes (Black)", "danger/spinners/Spooooky/AssetPack/musicnotes", "_black", false],
+    ["Music Notes (Purple)", "danger/spinners/Spooooky/AssetPack/musicnotes", "_purple", true],
+    ["Music Notes (Blue)", "danger/spinners/Spooooky/AssetPack/musicnotes", "_blue", true],
+    ["Music Notes (Teal)", "danger/spinners/Spooooky/AssetPack/musicnotes", "_teal", true],
+    ["Music Notes (Green)", "danger/spinners/Spooooky/AssetPack/musicnotes", "_green", true],
+    ["Music Notes (Yellow)", "danger/spinners/Spooooky/AssetPack/musicnotes", "_yellow", true],
+    ["Music Notes (Orange)", "danger/spinners/Spooooky/AssetPack/musicnotes", "_orange", true],
+    ["Music Notes (Red)", "danger/spinners/Spooooky/AssetPack/musicnotes", "_red", true],
+    ["Catstare", "danger/spinners/Spooooky/AssetPack/catstare", "_catstare", false],
+]
+
+function getEntity(path, suffix, drawOutline) {
+    // note: _id
+    return `{
+        {
+            _fromLayer = "entities",
+            _name = "FrostHelper/IceSpinner",
+            _type = "entity",
+            attachGroup = -1,
+            attachToSolid = false,
+            bloomAlpha = 0,
+            bloomRadius = 0,
+            borderColor = "000000",
+            collidable = true,
+            dashThrough = false,
+            debrisCount = 8,
+            destroyColor = "b0eaff",
+            directory = "${path}",
+            drawOutline = ${drawOutline},
+            moveWithWind = false,
+            rainbow = false,
+            singleFGImage = false,
+            spritePathSuffix = "${suffix}",
+            tint = "ffffff",
+            x = 320,
+            y = 120
+        }
+    }`
+}
+
+function buttonClick(button) {
+    info = spinners[button.id.substring(7)]; // 7 is length of text "button_"
+    navigator.clipboard.writeText(getEntity(info[1], info[2], info[3]));
+    button.innerHTML = "Copied!";
+    button.style.backgroundColor = "#e1bde0"
+    setDefaultColor(button)
+}
+
+async function setDefaultColor(button) {
+    await sleep(1000);
+    button.innerHTML = "Copy";
+    button.style.backgroundColor = null;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+for (let i = 0; i < spinners.length; i++) {
+    info = spinners[i];
+    //div for that spinner
+    let e = document.createElement("DIV");
+
+    //linebreak
+    let br = document.createElement("BR");
+
+    //title
+    let title = document.createElement("H2");
+    title.innerHTML = info[0];
+
+    //preview
+    let image = document.createElement("IMG");
+    image.setAttribute("src","./Images/spinners/" + info[0] + ".png");
+
+    //"copy" button
+    let button = document.createElement("BUTTON");
+    button.innerHTML = "Copy";
+    button.id = "button_" + i;
+    button.setAttribute("onclick","buttonClick(this)");
+        
+    e.append(title);
+    e.append(image);
+    e.append(br);
+    e.append(button);
+    document.body.append(e);
+}
